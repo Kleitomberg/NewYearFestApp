04d5ba61a65b1bf51af7e4fb75a613dc75f7c452
Kleitomberg <51670381+Kleitomberg@users.noreply.github.com>
2023-02-11 10:27:30 -0300

    logic dateof today and count daysleft to end year

--- app/src/main/java/br/dev/android/newyearfest/view/MainActivity.java
+++ app/src/main/java/br/dev/android/newyearfest/view/MainActivity.java
@@ -9,11 +9,15 @@ import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import br.dev.android.newyearfest.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private static SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.mViewHolder.confirmbtn = findViewById(R.id.btnConfirm);
 
         this.mViewHolder.confirmbtn.setOnClickListener(this);
+
+        //DATAS
+        this.mViewHolder.textToday.setText(SIMPLE_DATE_FORMAT.format(Calendar.getInstance().getTime()));
+
+        String daysleft = String.format("%s %s",
+            String.valueOf(this.getDaysLeft()),
+            getString(R.string.dias)
+        );
+
+        this.mViewHolder.valueDays.setText(daysleft);
+
 
     }
 
@@ -38,6 +53,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
 
+    }
+
+    private int getDaysLeft(){
+        // data hoje
+        Calendar calenderToday = Calendar.getInstance();
+        int hoje = calenderToday.get(Calendar.DAY_OF_YEAR);
+        // dias ano
+        Calendar calenderLastDay = Calendar.getInstance();
+        int maxDay = calenderLastDay.getActualMaximum(Calendar.DAY_OF_YEAR);
+
+        return maxDay - hoje;
     }
 
     private static class ViewHolder {
